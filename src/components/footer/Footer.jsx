ff5483cc8f57599aa935c1954d408ee5458a70e0
IgnatovOleg <08623.ignatovoleg@gmail.com>
2023-08-05 20:27:56 +0300

    dsfghgdsfa

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import cl from "./Footer.module.scss"
 import logoFooter from "../../img/logoFooter.png"
+import location from "../../img/location.svg"
+import phone from "../../img/phone.svg"
+import mail from "../../img/mail.svg"
+import fb from "../../img/fb.svg"
+import twitter from "../../img/twitter.svg"
+import linkedIn from "../../img/LinkedIn.svg"
+import pinterest from "../../img/pinterest.svg"
 
 const Footer = () => {
 
@@ -19,7 +26,23 @@ const Footer = () => {
                 </div>
                 <div className={cl.contactList}>
                     <h3>Контакты</h3>
+                    <span className={cl.location}><img src={location} />100000, Республика  Казахстан,
+                        г. Караганда, ул. Телевизионная 10</span>
+                    <span className={cl.phone}><img src={phone} /> +38 (000) 000 00 00</span>
+                    <span className={cl.mail}><img src={mail} />mail@gmail.com</span>
                 </div>
+                <div className={cl.socialMedia}>
+                    <h3>Социальные сети</h3>
+                    <div className={cl.icons}>
+                        <img src={fb} className={cl.fb} />
+                        <img src={twitter} className={cl.twitter} />
+                        <img src={linkedIn} className={cl.linkedIn} />
+                        <img src={pinterest} className={cl.pinterest} />
+                    </div>
+                </div>
+            </div>
+            <div className={cl.another}>
+                © <span> 2019</span> Digital Project. Все права защищены.
             </div>
         </div>
     )
